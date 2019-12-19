699059983310163ed5fdcbdf2f10e5f08310e5c4
Jeremie Roy <j-roy95@hotmail.com>
2019-12-18 20:27:11 -0500

    Add project

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 initalForce;
+    public List<Planet> planets;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(initalForce);
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("Planet");
+        foreach (GameObject obj in objects)
+        {
+            planets.Add(obj.GetComponent<Planet>());
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        foreach (Planet planet in planets)
+        {
+            var heading = this.transform.position - planet.transform.position;
+            heading /= -2*heading.magnitude;
+            
+            rb.AddForce(heading * planet.gravity);
+        }
+    }
+}
---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static System.Math;
+
+public class Planet : MonoBehaviour
+{
+    public float gravity;
+    public float maxGravity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        gravity = Min(transform.localScale.y, maxGravity); //(float) System.Math.Tanh(transform.localScale.y) * 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
